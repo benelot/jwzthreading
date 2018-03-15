385bf75b1f9217b5e747973667043b8fe2b0c4e0
Benelot <be.ellenberger@gmail.com>
2018-03-15 17:50:09 +0100

    Add some methods to get the thread length and a message index list.

--- jwzthreading/main.py
+++ jwzthreading/main.py
@@ -15,6 +15,28 @@ def print_container(ctr, depth=0, debug=0):
     for child in ctr.children:
         print_container(child, depth + 1, debug)
 
+
+def get_thread_length(ctr, depth=0):
+    base_list = get_thread_indices(ctr)
+    return len(base_list)
+
+
+def get_thread_indices(ctr):
+    base_list = []
+    return get_thread_indices_sub(ctr, base_list, 0)
+
+
+def get_thread_indices_sub(ctr, base_list, depth=0):
+    if ctr['message'] is not None:
+        message_id = ctr['message'].message_id
+        base_list.append(message_id)
+
+    for child in ctr.children:
+        get_thread_indices_sub(child, base_list, depth + 1)
+
+    return base_list
+
+
 def main():
     import mailbox
     import sys
@@ -33,9 +55,29 @@ def main():
     print('Threading...')
     threads = thread(msglist)
 
-    print('Output...')
+    # print('Output...')
+    # for container in threads:
+    #     print_container(container)
+
+    thread_length = {}
+
     for container in threads:
-        print_container(container)
+        length = get_thread_length(container)
+        if length + 1 not in thread_length:
+            thread_length[length + 1] = 1
+        else:
+            thread_length[length + 1] += 1
+
+    print("\n\nThread lengths:")
+    for i in sorted(thread_length):
+        print("Length:" + str(i + 1) + "   #:" + str(thread_length[i]))
+
+    idx_list = []
+    for container in threads:
+        thread_idx_list = get_thread_indices(container)
+        idx_list.append(thread_idx_list)
+
+    print("Total number of threads: ", len(idx_list))
 
 
 if __name__ == "__main__":
